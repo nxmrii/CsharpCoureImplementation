5c96f414e68f0a152e4388503a66aec544fea403
nxmrii <nalaamri19@gmail.com>
2026-06-01 16:58:56 +0400

    case 1 and 2 done, i understand it..

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,7 +2,68 @@
 {
     internal class Program
     {
-       
+
+        //case 1 -- Temperature Log
+        //array initilaization
+        static Double[] temperatures = {0.1,0.2,0.3,0.4,0.5,0.6,0.7};
+        static void temp() {
+            //for loop + indexing
+            for (int i = 0; i < temperatures.Length; i++) {
+
+                Console.WriteLine("Day " + (i + 1) + ":" + temperatures[i] + "c");
+            
+            }
+
+            Console.WriteLine("total number of stored tempretures: " + temperatures.Length);
+        }
+
+
+        //case 2 --  Student Score Board 
+        // store scores in array , initialize
+        static int[] scores = { 10, 20, 30, 40, 50, 100 };
+        static void studentScores()
+        {
+            foreach (int i in scores) {
+                Console.WriteLine("students scores is: " + i);
+            }
+
+            //Reverse the array and print it again.
+            Array.Reverse(scores);
+            Console.WriteLine("in reverse way: ");
+            for (int i = 0; i < scores.Length; i++) {
+                
+                Console.WriteLine(scores[i]);
+            }
+        }
+
+
+        // case 3 --  Product Price Finder 
+        static Double[] prices = { 50, 60.5, 30, 40.5, 70 };
+        static double prodictPrice(string key) { 
+        
+            for(int i = 0; i < prices.Length; i++)
+            {
+                Console.WriteLine("Product " + (i+1) + ":" + prices[i]);
+            }
+
+            Array.IndexOf(prices);
+
+                key = key;
+
+                // check if keyword insid title 
+                if (prices.Contains(key))
+                {
+                    return true;
+                }
+
+                return false;
+            
+        }
+
+
+
+
+
 
 
 
@@ -37,13 +98,15 @@
                 switch (choice)
                 {
                     case 1:
-                        
+                        temp();
                         break;
 
                     case 2:
+                        studentScores();
                         break;
 
                     case 3:
+                        prodictPrice();
                         break;
 
                     case 4:
