689b5a40ce92864ed251ca8ecf3ef4c2d827c940
nxmrii <nalaamri19@gmail.com>
2026-06-03 11:34:19 +0400

    new experience :)

---
+++ ListPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
